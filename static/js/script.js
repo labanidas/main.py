ca42a7dc57898ef825ec5738cffd6d15c5f86170
Labani <labanidas2001@gmail.com>
2023-09-18 12:09:57 +0530

    coding js

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,12 @@
 var sendButton = document.getElementById("sendButton");
 
 sendButton.addEventListener("click", () => {
-    que_input = document.getElementById("question");
-    question = que_input.value;
-    console.log(question)
+    que_input = document.getElementById("questionInput");
+    que = que_input.value;
     que_input.value = "";
+    document.querySelector(".right1").style.display = 'none';
+    document.querySelector(".right2").style.display = 'block';
 
-    // alert("hi")
+    document.querySelector("#question").innerHTML = que;
+
 })
