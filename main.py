699a69e4a8c418054b7f0e5fc18392c3ef359725
Labani <labanidas2001@gmail.com>
2023-09-21 17:19:40 +0530

    ajax req + env variable + dynamic render + ui refactor

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 from flask import Flask, render_template, request, jsonify
+import os
+import openai
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/api', methods = ['GET', 'POST'])
-def qa():
-    data = {
-            "request" : "thank you"
-        }
-    # if request.method == 'POST':       
-    #     return jsonify(data)
-    # return render_template('index.html')
-    return jsonify(data)
+
+# Set your OpenAI API key
+# openai.api_key = os.getenv("API_KEY")
+@app.route("/api", methods = ['GET', 'POST'])
+def fetch_data():
+    # access prompt question
+    prompt = request.form.get("prompt")
+    # response = openai.Completion.create(
+    #     engine="davinci",  # Choose the engine, e.g., "davinci" or "curie"
+    #     prompt="Translate the following English text to French: 'Hello, how are you?'",
+    #     max_tokens=50,  
+    # )
+
+    # generated_text = response.choices[0].text
+    # print(generated_text)
+
+    # Return the data as a JSON response
+    # return jsonify({"data": data})
+    data = "This is the response from the server."
+
+    # return data
+    # using jsonify({"data": data}) instead of return data since my cient side is expecting a json data instead of a single string
+    return jsonify({"data": data})
+    # return f"Server reponsed with 200 and promt is"
+
+
+
 
 app.run(debug = True)
+
+
+
+
--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,88 @@
-var sendButton = document.getElementById("sendButton");
 
-sendButton.addEventListener("click", () => {
-    que_input = document.getElementById("questionInput");
-    que = que_input.value;
-    que_input.value = "";
-    document.querySelector(".right1").style.display = 'none';
-    document.querySelector(".right2").style.display = 'block';
 
-    document.querySelector("#question").innerHTML = que;
+// var sendButton = document.getElementById("sendButton");
 
-})
+// sendButton.addEventListener("click", () => {
+//     que_input = document.getElementById("questionInput");
+//     que = que_input.value;
+//     que_input.value = "";
+//     document.querySelector(".right1").style.display = 'none';
+//     document.querySelector(".right2").style.display = 'block';
+
+//     document.querySelector("#question").innerHTML = que;
+// })
+
+// document.getElementById("askQuestion").addEventListener('click', () => {
+//     alert("hi")
+// })
+
+function render_user_message(userPrompt) {
+    let user_msg_box = `
+    <div class="box user box1 flex justify-start w-[70%] space-x-4 p-7 m-auto">
+    <img
+      class="w-9 h-9"
+      src="https://chat.openai.com/_next/image?url=https%3A%2F%2Flh3.googleusercontent.com%2Fa%2FAAcHTtf-3pmhALVSb-1WbSusjdPg_2UhrsZ-k88Nr8nRNUWOvZ0%3Ds96-c&w=48&q=75"
+      alt="user"
+    />
+    <div id="question">
+      ${userPrompt}
+    </div>
+  </div>
+    `
+
+    // Append the user message box to the div with id "right2"
+    document.querySelector(".allchats").innerHTML += user_msg_box;
+}
+
+function render_system_message(sysdata) {
+    let sys_msg_box = `
+    <div class="bg-gray-600 w-full bg chatgpt">
+          <div class="box box2 flex justify-start w-[70%] space-x-4 p-7 m-auto">
+            <img
+              class="w-9 h-9"
+              src="https://chat.openai.com/favicon.ico"
+              alt="ChatGPT"
+            />
+            <div class="flex flex-col">
+              <div>
+                ${sysdata}
+              </div>
+            </div>
+          </div>
+        </div>
+    `
+    // Append the user message box to the div with id "right2"
+    document.querySelector(".allchats").innerHTML += sys_msg_box;
+}
+
+
+// function to add scroll animation
+function scrollToBottom() {
+    var $allChats = $(".allchats");
+    $allChats.animate({ scrollTop: $allChats.prop("scrollHeight") }, 500);
+  }
+
+$("#askQuestion").click(function () {
+    // Get the user's input
+    var userPrompt = $("#questionInput2").val();
+    // render user message
+    render_user_message(userPrompt);
+    scrollToBottom();
+    $("#questionInput2").val('');
+
+    // Send an asynchronous request to your Flask server
+    $.ajax({
+        type: "POST", // Use POST or GET based on your server route
+        url: "/api",
+        data: { prompt: userPrompt },
+        success: function (response) {
+            // Update the UI with the response
+            render_system_message(response.data);
+            scrollToBottom();
+            // $("#right2").text(response.data);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+});
